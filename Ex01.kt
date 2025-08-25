395dfb7d302ca479e422f02ff62853b9bb3d6612
Aline Hannah <115451709+Enila03@users.noreply.github.com>
2025-08-25 01:29:27 -0300

    Add files via upload

---
+++ Ex01.kt
@@ -0,0 +1,18 @@
+fun main() {
+    var names: String = ""
+    var stop: String = ""
+
+    while (stop != "PARE") {
+        println("Digite o nome do aluno para cadastrar (ou PARE para encerrar):")
+        val add = readLine()!!.uppercase()
+
+        if (add == "PARE") { // compara o que o usuário digitou
+            stop = "PARE"
+        } else {
+            names += add + "\n"
+        }
+    }
+
+    println("Os alunos cadastrados sao:")
+    println(names)
+}
